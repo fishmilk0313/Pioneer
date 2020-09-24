bede8493a705c74ea6bd042d6c914ebba51fa550
vagrant <vagrant@localhost.localdomain>
2020-09-24 11:24:58 +0000

    rere

--- db/migrate/20200911151439_create_notifications.rb
+++ db/migrate/20200911151439_create_notifications.rb
@@ -1,12 +1,12 @@
 class CreateNotifications < ActiveRecord::Migration[5.2]
    def change
     create_table :notifications do |t|
-      t.references :visitor, foreign_key:{ to_table: :users }, null: false
-      t.references :visited, foreign_key:{ to_table: :users }, null: false
-      t.references :post, foregin_key: true
-      t.references :comment, foregin_key: true
-      t.references :room, foreign_key: true
-      t.references :message, foreign_key:true
+      t.references :visitor, foreign_key:{ to_table: :users }, null: false, type: :integer
+      t.references :visited, foreign_key:{ to_table: :users }, null: false, type: :integer
+      t.references :post, foregin_key: true, type: :integer
+      t.references :comment, foregin_key: true, type: :integer
+      t.references :room, foreign_key: true, type: :integer
+      t.references :message, foreign_key:true, type: :integer
       t.string :action, null: false
       t.boolean :checked, null: false, default: false
 
